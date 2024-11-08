bff41ab76a37d34a11d7b97973c056b481732546
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-11-09 01:56:09 +0200

    Add counter component using useState

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main style={{ padding: '20px', backgroundColor: 'blue' }}>
+        <main style={{ padding: '20px', backgroundColor: 'royalBlue' }}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
