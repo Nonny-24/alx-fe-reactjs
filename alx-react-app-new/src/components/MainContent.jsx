8a1adca21a82a831303d0962d57aaca20c8a4be4
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-11-09 01:29:50 +0200

    Add inline styling to new app components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main tyle={{ padding: '20px', backgroundColor: 'blue' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
