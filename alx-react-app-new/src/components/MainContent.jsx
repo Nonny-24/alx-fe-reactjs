c96c93f82a660c30abbddfd2a96602aee9059d34
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-11-09 01:33:58 +0200

    Add style tag to MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main tyle={{ padding: '20px', backgroundColor: 'blue' }}>
+        <main style={{ padding: '20px', backgroundColor: 'blue' }}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
