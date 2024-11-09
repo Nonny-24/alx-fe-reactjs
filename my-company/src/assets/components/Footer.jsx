bbc814d1181c65092438eef73b326ea4ec72dc0b
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-11-09 21:46:19 +0200

    Add my-company

---
+++ my-company/src/assets/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '20px', backgroundColor: '#333', color: '#fff', textAlign: 'center' }}>
+        <p>&copy; 2024 My Company. All Rights Reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
