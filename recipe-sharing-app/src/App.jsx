a078363e65131dd2fbe7f2577a5dc991a0bd7142
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-11-17 12:26:21 +0200

    Revert to origin

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <h1>Recipe Sharing Application</h1>
       <AddRecipeForm />
       <RecipeList />
-      <SearchBar />
     </div>
     </>
   )
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,29 +1,9 @@
 import create from 'zustand';
 
-/*const useRecipeStore = create(set => ({
+const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
-}));*/
-
-const useRecipeStore = create((set) => ({
-  recipes: [],
-  searchTerm: '',
-  filteredRecipes: [],
-  addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-      filteredRecipes: [...state.recipes, newRecipe].filter((recipe) =>
-        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-      ),
-    })),
-  setSearchTerm: (term) =>
-    set((state) => {
-      const filtered = state.recipes.filter((recipe) =>
-        recipe.title.toLowerCase().includes(term.toLowerCase())
-      );
-      return { searchTerm: term, filteredRecipes: filtered };
-    }),
 }));
 
 export default useRecipeStore;
