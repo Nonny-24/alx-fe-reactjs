548a3d5b03fb9d3c150e79f7fcfa0c2b3f014096
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-11-22 15:07:18 +0200

    Initial commit with RegistrationForm and FormikForm components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import Home from './components/Home'; 
-import EditRecipeForm from '.components/EditRecipeForm';
-import SearchBar from '.components/SearchBar';
+import Home from './components/Home';
+import EditRecipeForm from './components/EditRecipeForm';
+import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
 
