f8cdf5ada9c39e7e9c0e7279c2b4da5ea05824e2
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-11-28 10:25:21 +0200

    Import ProtectedRoute.jsx to App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import BlogPost from './components/BlogPost'; 
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
