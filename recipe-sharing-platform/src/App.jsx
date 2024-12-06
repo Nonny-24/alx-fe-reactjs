7beaf469d8eb0cd8234085160bc8f4b4540ac11d
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-12-06 16:54:56 +0200

    Create Homepage for recipe sharing platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 
 function App() {
   
   return (
     <>
-      <p className='text-blue-500 font-bold'>
+      <p className='text-blue-500'>
       Setting up a react application for a recipe sharing platform with Tailwind CSS
       </p>
+      <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          {/* Route for the Home Page */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* Placeholder for other routes like Recipe Details */}
+          <Route
+            path="/recipe/:id"
+            element={<div className="text-center text-gray-700 mt-20">Recipe Details Coming Soon!</div>}
+          />
+        </Routes>
+      </div>
+    </Router>
     </>
   )
 }
