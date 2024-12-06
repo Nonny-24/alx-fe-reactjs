4d8fe8a84bd7541eef2e13deb3048791039bcdd8
Nonhlanhla Mokoena <mokoena3586@gmail.com>
2024-12-06 16:30:52 +0200

    Setting up new react application with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <p className='text-blue-500 font-bold'>
+      Setting up a react application for a recipe sharing platform with Tailwind CSS
+      </p>
+    </>
+  )
+}
+
+export default App
